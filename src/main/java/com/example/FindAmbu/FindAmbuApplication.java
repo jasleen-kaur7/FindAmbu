8e895a43863735acf123cacdbf43981953cfbd19
jasleen-kaur7 <jasleen.kaur7k@gmail.com>
2021-09-12 03:23:21 +0530

    First commit

---
+++ src/main/java/com/example/FindAmbu/FindAmbuApplication.java
@@ -0,0 +1,13 @@
+package com.example.FindAmbu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindAmbuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindAmbuApplication.class, args);
+	}
+
+}
